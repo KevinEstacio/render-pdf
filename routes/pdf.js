7d86fdeb5ff327db32f9d43fefb9922b524a9f96
Kevin Estacio <knontole15_2@unc.edu.pe>
2024-03-29 23:54:28 -0500

    cambios en path

--- routes/pdf.js
+++ routes/pdf.js
@@ -9,37 +9,26 @@ router.post("/", async (req, res, next) => {
   const { body, headers } = req;
   const { html, options } = body;
   let file_name = `${uniqid()}.pdf`, existe = "";
-  const file_path = `https://render-pdf-we6j.onrender.com/public/storage/pdf/${file_name}`;
+  const file_path = `./public/storage/pdf/${file_name}`;
 
-  console.log(file_path)
   pdf.create(html, options).toFile(file_path, (err, stream) => {
 
-    if (err) {
-      console.error("Error al crear el archivo PDF:", err);
-      return res.status(500).send({
-        status: false,
-        message: "Error al crear el archivo PDF",
-        error: err
-      });
-    }
       try {
         existe = fs.existsSync(`./public/storage/pdf/${file_name}`);
-        fs.readdir( './public/storage/pdf/',(error,archivos) =>{
-          console.log(archivos)
-        } )
         res.send({
           status: true,
           exists: existe,
           message: "PDF Rutas Contenedor",
           base: `./public/storage/pdf/${file_name}`,
-          data: `${process.env.NODE_PRODUCTION === "true" ? process.env.API_PRODUCCION : process.env.API}/storage/pdf/${file_name}`,
-          download: `${process.env.NODE_PRODUCTION === "true" ? process.env.API_PRODUCCION : process.env.API}/download?public/storage/pdf/${file_name}`,
+          data: `https://render-pdf-we6j.onrender.com/storage/pdf/${file_name}`,
+          download: `https://render-pdf-we6j.onrender.com/download?public/storage/pdf/${file_name}`,
         });
       } catch (error) {
         res.send({
           status: false,
           message: "PDF Rutas Null",
           data: null,
+          error: error
         });
       }
     });
